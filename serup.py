48786c080329a562cc0268174ac8a5b6950236e2
fynk7777 bakabonnpapa <fynk7777@yahoo.co.jp>
2024-09-02 11:25:40 +0900

    Update serup.py

--- serup.py
+++ serup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, Extension
 
-module = Extension("factorizer", sources=["factorizer.c"])
+module = Extension('factorizer', sources=['factorizer.c'])
 
 setup(
-    name="factorizer",
-    version="1.0",
-    description="A Python module for prime factorization",
+    name='factorizer',
+    version='1.0',
+    description='A Python C extension for prime factorization',
     ext_modules=[module],
 )
